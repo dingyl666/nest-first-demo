548f115097e1759190219b5340ea3af9220b305f
dingyl666 <1811239843@qq.com>
2023-09-16 00:55:43 +0800

    继续练习

--- src/modules/user/user/user.controller.ts
+++ src/modules/user/user/user.controller.ts
@@ -1,33 +1,45 @@
-import { Body, Controller, Get,Post, Query } from "@nestjs/common";
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 
-import { DataModel, UserService } from "./user.service" ;
+import { DataModel, UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService:UserService) {}
+  constructor(private readonly userService: UserService) {
+    //private readonly userService: UserService是个语法糖
+    //等价于 this.userService = new UserService()
+  }
+
+  @Get('/test')
+  test() {
+    return '111';
+  }
+  @Post('/test2')
+  test2() {
+    return '222';
+  }
 
   @Get('/getList')
-  getUserList(){
-    return this.userService.getUserList()
+  getUserList() {
+    return this.userService.getUserList();
   }
 
   @Get()
-  getUserById(@Query('userId') userId:number) {
-    return this.userService.getUserById(Number(userId))
+  getUserById(@Query('userId') userId: number) {
+    return this.userService.getUserById(Number(userId));
   }
 
   @Post('/add')
-  addUser(@Body() body:DataModel) {
-    return this.userService.addUser(body) ;
+  addUser(@Body() body: DataModel) {
+    return this.userService.addUser(body);
   }
 
   @Post('/del')
-  delUser(@Body('userId') userId:number) {
-    return this.userService.delUser(Number(userId)) ;
+  delUser(@Body('userId') userId: number) {
+    return this.userService.delUser(Number(userId));
   }
 
   @Post('/up')
-  upUser(@Body('userId') userId:number,@Body('name')name:string) {
-    return this.userService.upUser(Number(userId),name) ;
+  upUser(@Body('userId') userId: number, @Body('name') name: string) {
+    return this.userService.upUser(Number(userId), name);
   }
 }
--- src/modules/user/user/user.service.ts
+++ src/modules/user/user/user.service.ts
@@ -1,45 +1,44 @@
 import { Injectable } from '@nestjs/common';
 
 export class DataModel {
-  userId = 0 ;
-  name = '' ;
+  userId = 0;
+  name = '';
 
-  constructor(id=0,name = '') {
-    this.userId = id ;
-    this.name = name ;
+  constructor(id = 0, name = '') {
+    this.userId = id;
+    this.name = name;
   }
 }
-let list = [new DataModel(0,'测试数据')] as DataModel[] ;
+let list = [new DataModel(0, '测试数据')] as DataModel[];
 @Injectable()
 export class UserService {
-
-  getUserById(userId:number){
-    return list.find(dd => dd.userId === userId) ?? new DataModel() ;
+  getUserById(userId: number) {
+    return list.find((dd) => dd.userId === userId) ?? new DataModel();
   }
 
   getUserList() {
-    return list
+    return list;
   }
 
-  addUser(data:DataModel) {
-    if(data.userId) {
-      list.push(data) ;
+  addUser(data: DataModel) {
+    if (data.userId) {
+      list.push(data);
     }
-    return { info:"success" } ;
+    return { info: 'success' };
   }
 
-  delUser(id:number) {
-    if(id && id>0) {
-      list = list.filter(ff => ff.userId !== id) ;
+  delUser(id: number) {
+    if (id && id > 0) {
+      list = list.filter((ff) => ff.userId !== id);
     }
-    return { info:"success" } ;
+    return { info: 'success' };
   }
 
-  upUser(id:number,name:string) {
-    const find = list.find(dd => dd.userId === id) ;
-    if(find) {
-      find.name = name ;
+  upUser(id: number, name: string) {
+    const find = list.find((dd) => dd.userId === id);
+    if (find) {
+      find.name = name;
     }
-    return { info:"success" } ;
+    return { info: 'success' };
   }
 }
