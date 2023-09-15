ac475b34abe0aa02bc948497c3361194b1cfc223
‘dingyl666’ <1811239843@qq.com>
2023-09-15 17:59:58 +0800

    user crud 练习

--- src/modules/user/user/user.controller.ts
+++ src/modules/user/user/user.controller.ts
@@ -1,12 +1,33 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { Body, Controller, Get,Post, Query } from "@nestjs/common";
 
-import {UserService} from './user.service'
+import { DataModel, UserService } from "./user.service" ;
+
 @Controller('user')
 export class UserController {
   constructor(private readonly userService:UserService) {}
 
-  @Get(':userId')
-  getUserById(@Param('userId') userId:string) {
-    return this.userService.getUserById(userId)
+  @Get('/getList')
+  getUserList(){
+    return this.userService.getUserList()
+  }
+
+  @Get()
+  getUserById(@Query('userId') userId:number) {
+    return this.userService.getUserById(Number(userId))
+  }
+
+  @Post('/add')
+  addUser(@Body() body:DataModel) {
+    return this.userService.addUser(body) ;
+  }
+
+  @Post('/del')
+  delUser(@Body('userId') userId:number) {
+    return this.userService.delUser(Number(userId)) ;
+  }
+
+  @Post('/up')
+  upUser(@Body('userId') userId:number,@Body('name')name:string) {
+    return this.userService.upUser(Number(userId),name) ;
   }
 }
--- src/modules/user/user/user.service.ts
+++ src/modules/user/user/user.service.ts
@@ -1,8 +1,45 @@
 import { Injectable } from '@nestjs/common';
 
+export class DataModel {
+  userId = 0 ;
+  name = '' ;
+
+  constructor(id=0,name = '') {
+    this.userId = id ;
+    this.name = name ;
+  }
+}
+let list = [new DataModel(0,'测试数据')] as DataModel[] ;
 @Injectable()
 export class UserService {
-  getUserById(userId:string){
-    return  `当前用户id:${userId}`
+
+  getUserById(userId:number){
+    return list.find(dd => dd.userId === userId) ?? new DataModel() ;
+  }
+
+  getUserList() {
+    return list
+  }
+
+  addUser(data:DataModel) {
+    if(data.userId) {
+      list.push(data) ;
+    }
+    return { info:"success" } ;
+  }
+
+  delUser(id:number) {
+    if(id && id>0) {
+      list = list.filter(ff => ff.userId !== id) ;
+    }
+    return { info:"success" } ;
+  }
+
+  upUser(id:number,name:string) {
+    const find = list.find(dd => dd.userId === id) ;
+    if(find) {
+      find.name = name ;
+    }
+    return { info:"success" } ;
   }
 }
