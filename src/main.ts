dd70037fcce76b23f961e85ec02b5bd8ca5c4e46
‘dingyl666’ <1811239843@qq.com>
2023-09-19 12:00:52 +0800

    定时任务

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule,{cors:true});
+  // app.enableCors();
   app.setGlobalPrefix('api/v1');
   await app.listen(8888);
 }
